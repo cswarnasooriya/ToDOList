f25efa87385fb43f448cdfa5b5f0bd94f06bfe9a
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-04-19 22:55:34 +0530

    Home updated

---
+++ todolist/src/Home.jsx
@@ -0,0 +1,29 @@
+import Create from './Create'
+import { useState } from 'react'
+const Home = () => {
+
+    const [todo,setTodo] = useState([]);
+
+  return (
+    <div>
+      <h2>ToDo List</h2>
+      <Create/>
+
+      {
+
+        todo.length === 0
+        ?
+        <div><h2>No Record</h2></div>
+        :
+        todo.map(todo => (
+            <div>
+                {todo}
+            </div>    
+        ))
+
+      }
+    </div>
+  )
+}
+
+export default Home
